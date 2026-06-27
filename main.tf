b25b0ca3a38d6c6c6f8311db88b84fd5db356f65
jagadandivarshitha-cmd <jagadandivarshitha@gmail.com>
2026-06-27 10:06:07 +0530

    Terraform - EC2 + Security Group deployment

--- main.tf
+++ main.tf
@@ -17,3 +17,33 @@ resource "aws_s3_bucket" "my_bucket"{
         Environment = var.environment
     }
 }
+
+resource "aws_security_group" "my_sg" {
+    name = "my-server-sg"
+    description = "Security group for my first server"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]     # allow ssh from anywhere
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"    # all traffic 
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "my_server"{
+    ami = "ami-0521cb2d60cfbb1a6"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+    tags = {
+        Name = "MyFirstServer"
+        Environment = var.environment
+    }
+}
--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,13 @@ output "bucket_arn" {
     description = "The ARN of the created S3 bucket"
     value = aws_s3_bucket.my_bucket.arn
 }
+
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.my_server.id
+}
+
+output "instance_public_ip" {
+  description = "EC2 Instance Public IP"
+  value       = aws_instance.my_server.public_ip
+}
